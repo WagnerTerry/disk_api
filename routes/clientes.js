dcb8762ac29568a3804dbda003cf41ab8d68c77b
WagnerTerry <wagner.goncalves@calindra.com.br>
2021-10-31 17:14:11 -0300

    post clientes

--- routes/clientes.js
+++ routes/clientes.js
@@ -32,4 +32,47 @@ router.get("/", (req, res, next) => {
   });
 });
 
+router.post("/cadastro", (req, res, next) => {
+  mysql.getConnection((error, conn) => {
+    if (error) {
+      return res.status(500).send({ error });
+    }
+    conn.query(
+      `insert into Clientes
+      (nome, telefone, cep, logradouro, bairro, cidade, observacoes)
+      values (?,?,?,?,?,?,?);
+      `,
+      [
+        req.body.nome,
+        req.body.telefone,
+        req.body.cep,
+        req.body.logradouro,
+        req.body.bairro,
+        req.body.cidade,
+        req.body.observacoes,
+      ],
+      (error, results) => {
+        conn.release();
+        if (error) {
+          return res.status(500).send({ error });
+        }
+        const response = {
+          mensagem: "Cliente cadastrado com sucesso!",
+          clienteCadastrado: {
+            codigo_cliente: results.insertId,
+            nome: req.body.nome,
+            telefone: req.body.telefone,
+            cep: req.body.cep,
+            logradouro: req.body.logradouro,
+            bairro: req.body.bairro,
+            cidade: req.body.cidade,
+            observacoes: req.body.observacoes,
+          },
+        };
+        return res.status(201).send(response);
+      }
+    );
+  });
+});
+
 module.exports = router;
