17d8d5e74848bd5beee4bb3219b2d664c210e02a
Wagner Terry <goncalveswagner15@gmail.com>
2022-01-20 12:13:32 -0300

    corrigindo erro de router

--- routes/caixa.js
+++ routes/caixa.js
@@ -11,29 +11,31 @@ router.get("/", (req, res, next) => {
       `
     select * from Caixa
     `,
-      (err, results) => {
+      (error, results) => {
         conn.release();
-        if (err) {
-          return res.status(500).send({ err });
+        if (error) {
+          return res.status(500).send({ error });
         }
         const response = {
-          quantidade_pedido = results.length,
+          quantidade_pedido: results.length,
           fluxo_caixa: results.map((caixa) => {
-              return {
-                  codigo_pedido: caixa.codigo_pedido,
-                  data: caixa.datas,
-                  hora: caixa.hora,
-                  cliente: caixa.nome_cliente,
-                  pizza: caixa.nome_pizza,
-                  bairro: caixa.bairro,
-                  entregador: caixa.entregador,
-                  situacao: caixa.situacao,
-                  valor: caixa.valor
-              }
-          })
+            return {
+              codigo_pedido: caixa.codigo_pedido,
+              data: caixa.datas,
+              hora: caixa.hora,
+              cliente: caixa.nome_cliente,
+              pizza: caixa.nome_pizza,
+              bairro: caixa.bairro,
+              entregador: caixa.entregador,
+              situacao: caixa.situacao,
+              valor: caixa.valor,
+            };
+          }),
         };
         return res.status(200).send({ response });
       }
     );
   });
 });
+
+module.exports = router;
