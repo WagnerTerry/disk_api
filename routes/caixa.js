5580f0f7e9b243bca65b647498dbf89445eefe73
Wagner Terry <goncalveswagner15@gmail.com>
2022-01-21 13:36:22 -0300

    post para criar os registros do caixa

--- routes/caixa.js
+++ routes/caixa.js
@@ -32,7 +32,53 @@ router.get("/", (req, res, next) => {
             };
           }),
         };
-        return res.status(200).send( response );
+        return res.status(200).send(response);
+      }
+    );
+  });
+});
+
+router.post("/", (req, res, next) => {
+  mysql.getConnection((error, conn) => {
+    if (error) {
+      return res.status(500).send({ error });
+    }
+    conn.query(
+      `
+      insert into Caixa
+      (datas, hora, nome_cliente, nome_pizza, bairro, entregador, situacao, valor)
+      values (?,?,?,?,?,?,?,?)
+      `,
+      [
+        req.body.datas,
+        req.body.hora,
+        req.body.nome_cliente,
+        req.body.nome_pizza,
+        req.body.bairro,
+        req.body.entregador,
+        req.body.situacao,
+        req.body.valor,
+      ],
+      (error, results) => {
+        conn.release();
+        if (error) {
+          return res.status(500).send({ response });
+        }
+        const response = {
+          mensagem: "Caixa salva com sucesso",
+          registroCadastrado: {
+            codigo_caixa: results.insertId,
+            datas: req.body.datas,
+            hora: req.body.hora,
+            nome_cliente: req.body.nome_cliente,
+            nome_pizza: req.body.nome_pizza,
+            bairro: req.body.bairro,
+            entregador: req.body.entregador,
+            situacao: req.body.situacao,
+            valor: req.body.valor,
+          },
+        };
+        return res.status(201).send(response);
       }
     );
   });
